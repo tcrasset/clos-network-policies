9f2064fb5d36da03333290252a821533a349d646
tcrasset <tomcrasset@gmail.com>
2019-11-30 13:26:50 +0100

    Add documentation

--- tenants.py
+++ tenants.py
@@ -2,14 +2,59 @@
 from pox.core import core
 
 class Tenants(object):
+    """Object holding information about hosts and tenants in a VLAN.
+    
+    Arguments
+    ----------
+    self.vlans: dict of EthAddr: int
+        Mapping of MAC_Address to vlan_id
+
+    """
     def __init__(self, n_vlans):
+        """Initializes the Tree_Controller object.
+
+        Parameters
+        ----------
+        n_vlans : int
+            Total number of different VLANs
+        """
+
         self.vlans = {}
         self.n_vlans = n_vlans
 
-    def addToVLAN(self, address, out_port):
-        self.vlans[address] = out_port
+    def addToVLAN(self, address, vlan_id):
+        """Adds a mapping to from MAC address to vlan_id.
+
+        Parameters
+        ----------
+        address : EthAddr
+            MAC_Address of a host
+        vlan_id : int
+            ID specifying to which tenant a host belongs
+        
+        Returns
+        ----------
+        None
+        """
+
+        self.vlans[address] = vlan_id
+        return
 
     def getVLAN(self, address):
+        """Returns VLAN ID of a given host MAC address.
+
+        Parameters
+        ----------
+        address : EthAddr
+            MAC_Address of a host
+        
+        Returns
+        ----------
+        int
+            The vlan_id associated to the address or
+            -1 if None is found.
+        """
+
         if(address in self.vlans):
             return self.vlans[address]
         else:
